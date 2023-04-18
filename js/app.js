5efd5c32b94b76de68fbb5ea8691468fb845374e
claudiomonzoni <claudiomonzoni@hotmail.com>
2023-04-18 09:46:26 -0600

    agregando el formulario

--- js/app.js
+++ js/app.js
@@ -14,7 +14,7 @@ tl.from ("#izq",{
   opacity: 0,
   duration: 1 
 }, "-=.7")
-tl.from ("#der h3, #der input, #der select, #der a",{
+tl.from ("#der h3, #der input, #der select, #der button",{
   stagger:0.1,
   y:100,
   opacity: 0,
@@ -80,50 +80,25 @@ function bg() {
 }
 bg()
 
+const handleSubmit = (event) => {
+  event.preventDefault();
 
-  window.$ = require('jquery');
+  const myForm = event.target;
+  const formData = new FormData(myForm);
+  
+  fetch("/", {
+    method: "POST",
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: new URLSearchParams(formData).toString(),
+  })
+    .then(() => console.log("Form successfully submitted"))
+    .catch((error) => alert(error));
+};
 
-$(document).ready(function () {
+document
+  .querySelector("form")
+  .addEventListener("submit", handleSubmit);
 
-$("#contactoBtn").click(function (e) {
-
-  var nombre = $("#nombre").val();
-    var apellido = $("#apellido").val();
-    var email = $("#email").val();
-    var tel = $("#tel").val();
-    var curso = $("#cursos").val();
-    var edo = $("#edo").val();
-    var ciudad = $("#ciudad").val();
-
-    $("#msj").empty(); // To empty previous error/success message.
-    // Checking for blank fields.
-
-    if (nombre == "" || apellido == "" || curso == ""|| tel == "" ||  email == "") {
-      alert("Por favor llena los campos marcados con * al final");
-    } else {
-      
-
-      $.post(
-        "contacto.php",
-        {
-          nombre1: nombre, apellido1: apellido, email1: email, tel1: tel,  curso1: curso, edo1: edo, ciudad1: ciudad
-        },
-        function (data) {
-          $("#msj").append(data); // Append returned message to message paragraph.
-          if (
-            data ==
-            "Gracias, pronto un asesor se pondra en contacto contigo 😎"
-          ) {
-            $("#formu").reset(); // To reset form fields on success.
-          }
-        }
-      );
-    }
-
-    e.preventDefault();
-  });
-
-})
 // cargar modulos solo por secciones
 // switch (document.location.pathname)
 // {
